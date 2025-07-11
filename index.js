bbae8e382d4112865ce8f004a008d32c7f299128
PavatharaniV <96381240+PavatharaniV@users.noreply.github.com>
2025-07-12 01:35:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -24,4 +24,6 @@ app.get('/', (req, res) => {
   res.send('Hello from the root route!');
 });
 
-app.listen(3000, () => console.log('Server running on port 3000'));
+app.listen(3000, '0.0.0.0', () => {
+  console.log('Server running on port 3000');
+});
