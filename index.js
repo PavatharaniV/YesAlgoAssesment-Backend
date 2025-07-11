a060463936d91c8ef10b27298220a15f1799cdd8
PavatharaniV <96381240+PavatharaniV@users.noreply.github.com>
2025-07-12 00:57:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -24,4 +24,6 @@ app.get('/', (req, res) => {
   res.send('Hello from the root route!');
 });
 
+console.log(process.env.MONGODB_URI);
+
 app.listen(3000, () => console.log('Server running on port 3000'));
